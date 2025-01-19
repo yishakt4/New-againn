89a6282d1612ceee3130267e9802623ffff13793
Amanuel Isayas <Halieseliase1936@gmail.com>
2025-01-19 23:18:28 +0300

    hope

--- util/database.js
+++ util/database.js
@@ -8,3 +8,14 @@ db.connect((err) => {
   }
 });
 
+const mysql = require('mysql2/promise');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Emp#136y',
+  database: 'Authentication'
+});
+
+const [rows, fields] = await db.execute('SELECT * FROM products');
+console.log(rows);
